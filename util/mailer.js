75c7e755fde19df677b5c879e1a066c005776198
Soze <mohitlkr01@gmail.com>
2023-10-01 02:20:07 +0530

    few changes done

--- util/mailer.js
+++ util/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service:'gmail',
     auth: {
-        user: "tdemo651@gmail.com",
-        pass: "tedihyjmypzstvwq"
+        user: "your@email.com",
+        pass: "emailPass"
     },
     port: 465,
     host: 'smtp.gmail.com'
